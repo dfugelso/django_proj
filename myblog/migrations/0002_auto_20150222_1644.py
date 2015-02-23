d347960e612f442b9efa660dfba156519a5f2889
Dave Fugelso <dfugelso@swcp.com>
2015-02-22 20:04:18 -0800

    Initial commit. Not working atm.

---
+++ myblog/migrations/0002_auto_20150222_1644.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('myblog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=128)),
+                ('description', models.TextField(blank=True)),
+                ('posts', models.ManyToManyField(related_name='categories', null=True, to='myblog.Post', blank=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='post',
+            name='text',
+            field=models.TextField(blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='post',
+            name='title',
+            field=models.CharField(default=datetime.datetime(2015, 2, 23, 0, 44, 13, 899000, tzinfo=utc), max_length=128),
+            preserve_default=False,
+        ),
+    ]
