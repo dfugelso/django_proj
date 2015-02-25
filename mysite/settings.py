1b80d24af1b3376e48751fdc0f941c010bdac584
Dave Fugelso <dfugelso@swcp.com>
2015-02-24 20:53:52 -0800

    Tuesday night class changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'mysite/templates'), )
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -82,3 +82,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
